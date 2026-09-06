03eaee0a26541e767f58936238246fad307e7366
MohammadFahad1 <fahad4bangladesh@gmail.com>
2026-09-06 11:54:48 +0600

    URL Fixed

--- apps/accounts/urls.py
+++ apps/accounts/urls.py
@@ -3,7 +3,7 @@ from accounts import views
 
 urlpatterns = [
     path('signup/', views.UserSignUpView.as_view(), name='signup'),
-    path('resend-otp/activate/', views.ResendActivationEmailAPIView.as_view(), name='resend-otp-activation'),
+    path('resend-otp/activation/', views.ResendActivationEmailAPIView.as_view(), name='resend-otp-activation'),
     path('verify-email/', views.VerifyEmailAddressAPIView.as_view(), name='verify-email'),
     path('login/', views.LoginAPIView.as_view(), name='verify-email'),
 ]
