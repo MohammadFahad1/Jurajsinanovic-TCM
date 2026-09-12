e178373d6dbac3036a7fcad609b483f0f7eed727
MohammadFahad1 <fahad4bangladesh@gmail.com>
2026-09-12 10:37:51 +0600

    Health check question and answer model, serializer designed

--- apps/health_check/models.py
+++ apps/health_check/models.py
@@ -1,3 +1,28 @@
 from django.db import models
 
-# Create your models here.
+class Question(models.Model):
+    title = models.CharField(max_length=500)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return self.title
+
+    class Meta:
+        verbose_name = 'Question'
+        verbose_name_plural = 'Questions'
+
+class Answer(models.Model):
+    question = models.ForeignKey(Question, on_delete=models.CASCADE, related_name='answers')
+    answer = models.CharField(max_length=500)
+    score = models.IntegerField(default=0)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return self.answer
+
+    class Meta:
+        verbose_name = 'Answer'
+        verbose_name_plural = 'Answers'
+
--- apps/health_check/serializers.py
+++ apps/health_check/serializers.py
@@ -1,3 +1,17 @@
 from rest_framework import serializers
+from health_check.models import Question, Answer
+
+class AnswerSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Answer
+        fields = ['id', 'answer', 'score', 'created_at', 'updated_at']
+
+
+class QuestionSerializer(serializers.ModelSerializer):
+    answers = AnswerSerializer(many=True, read_only=True)
+
+    class Meta:
+        model = Question
+        fields = ['id', 'title', 'answers', 'created_at', 'updated_at']
 
 
