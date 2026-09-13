c95ad68692a759586c34c1cba8d00c817dd20e10
MohammadFahad1 <fahad4bangladesh@gmail.com>
2026-09-13 11:19:44 +0600

    Payment model created

--- apps/accounts/models.py
+++ apps/accounts/models.py
@@ -107,4 +107,29 @@ class HealthProfile(models.Model):
         verbose_name = 'Health Profile'
         verbose_name_plural = 'Health Profiles'
 
+class Payment(models.Model):
+    SUCCESSFUL = 'successful'
+    PENDING = 'pending'
+    FAILED = 'failed'
+    STATUS_CHOICES = [
+        (SUCCESSFUL, 'Successful'),
+        (PENDING, 'Pending'),
+        (FAILED, 'Failed'),
+    ]
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    plan = models.ForeignKey(Plan, on_delete=models.SET_NULL, null=True, blank=True)
+    amount = models.DecimalField(max_digits=10, decimal_places=2)
+    payment_method = models.CharField(max_length=50)
+    transaction_id = models.CharField(max_length=100, unique=True)
+    payment_date = models.DateTimeField(auto_now_add=True)
+    status = models.CharField(max_length=20, choices=STATUS_CHOICES, default=PENDING)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+    
+    def __str__(self):
+        return f"Payment #{self.pk} - {self.user.email} - ${self.amount} - {self.status}"
+
+
+
+
 
