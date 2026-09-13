abb72d04336f27545bd84622854729155bc86f4d
MohammadFahad1 <fahad4bangladesh@gmail.com>
2026-09-13 12:08:42 +0600

    permission file updated

--- core/permissions.py
+++ core/permissions.py
@@ -1,26 +1,22 @@
+import requests_oauthlib
 from django.contrib.auth import get_user_model
 from rest_framework import permissions
+from django.utils import timezone
 
 User = get_user_model()
 
-class IsOwnerOrReadOnly(permissions.BasePermission):
-    """
-    Custom permission to only allow owners of an object to edit it.
-    """
-
-    def has_object_permission(self, request, view, obj):
-        # Read permissions are allowed to any request,
-        # so we'll always allow GET, HEAD or OPTIONS requests.
-        if request.method in permissions.SAFE_METHODS:
-            return True
-
-        # Write permissions are only allowed to the owner of the object.
-        return obj.owner == request.user
-
+class IsSubscribedUser(permissions.BasePermission):
     def has_permission(self, request, view):
-        # Allow read-only access for any request
-        if request.method in permissions.SAFE_METHODS:
+        user = request.user
+        if not user or not user.is_authenticated:
+            return False
+        
+        if user.is_superuser:
             return True
 
-        # Allow write access only for authenticated users
-        return request.user and request.user.is_authenticated
+        if not user.plan or user.plan and user.plan_end_date < timezone.now():
+            return False
+
+        return True
+        
+        
